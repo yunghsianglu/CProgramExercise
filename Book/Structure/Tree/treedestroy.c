6e16c1d0e8a914eaee4c78d9f1db1d3b6dfaed7b
yunglu <yunglu@yunglu-Latitude-E6320.(none)>
2014-03-16 15:22:12 -0400

    add examples

---
+++ Book/Structure/Tree/treedestroy.c
@@ -0,0 +1,13 @@
+#include "tree.h"
+#include <stdlib.h>
+void Tree_destroy(TreeNode * n)
+{
+  if (n == NULL) 
+    {
+      return;
+    }
+  Tree_destroy(n -> left);
+  Tree_destroy(n -> right);
+  free(n);
+}
+
